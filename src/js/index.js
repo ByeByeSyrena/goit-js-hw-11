d4591128d22a94fd8a4415d798cad1d8b7f933a7
Ira S <iryna.shevchenko2@gmail.com>
2023-08-16 22:22:49 +0300

    in pr

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,10 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.getElementById("search-form");
-const input = form.elements.searchQuery;
+let input = form.elements.searchQuery;
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = "38857854-067aa7c8dbb389fbf2efb7da8"
-
-let array = null;
 
 async function getGallery() {
   try {
@@ -20,34 +18,37 @@ async function getGallery() {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        p: `EncodeURIComponent('${input.value}')`,
+        p: input.value,
       }
     });
-    return response.hits;
+    return response;
   }
   catch(error) {
     console.log("error");
 }
 };
 
-getGallery();
+// getGallery();
 
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
 
-  if (event.target !== BUTTON) {
+  event.preventDefault();
+  const searchKeywords = input.value.toLowerCase().split(" ").join("+");
+  
+  input.value = searchKeywords;
+
+  if (!searchKeywords || searchKeywords == "") {
+    Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
     return;
-  } else if (input.value =""){
-    return Notiflix.Notify.warning('Please fill in this field');
   };
 
-  getGallery(input.value);
-
-  // else if (input = "" || input !== ) { // тут має бути ім'я елементу масиву
-  //   Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-  // }
-
-
+    try {
+    const galleryData = await getGallery(searchKeywords);
+    console.log(galleryData);
+  } catch (error) {
+    console.error('Error:', error);
+  };
 });
 
 
